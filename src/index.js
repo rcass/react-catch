e59bf0c4c1f6ad48f00801835439857b1a6e6e44
Rose Cass <rose.cass12@gmail.com>
2017-11-02 09:41:53 -0700

    Adding react router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 
+import NotFound from './components/NotFound';
 import Inventory from './components/Inventory';
 import Order from './components/Order';
 import Fish from './components/Fish';
 import Header from './components/Header';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+
+
 
 render(<App />, document.querySelector('#main'));
