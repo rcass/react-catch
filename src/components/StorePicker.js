f3438b2e524e1907d1e6f590ad3bc79d82a7e1dd
Rose Cass <rose.cass12@gmail.com>
2017-11-04 11:33:16 -0700

    Add routing to store picker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event){
+    event.preventDefault();
+    
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+
+  }
   render () {
     return(
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input)=> { this.storeInput = input }} />
         <button type="submit">Visit Store ▶</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
